db97c499c123ebfd3f8d0e88dd4cb64525faa452
AntonyBil <beletskiy.ant@gmail.com>
2023-09-24 16:45:58 +0300

    Initial commit

---
+++ CatchPokemon/CatchPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchPokemonApp.swift
+//  CatchPokemon
+//
+//  Created by apple on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatchPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
